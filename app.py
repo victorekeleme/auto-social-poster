2cc98d485e017ed5c87c77eebb6128fdf504dd6e
Victor Ekeleme <ekelemevictor99@gmail.com>
2024-03-29 00:18:26 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,182 +1,134 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file, flash, session
-from werkzeug.utils import secure_filename
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
-from pdf2docx import Converter
 from flask_socketio import SocketIO, emit
 import asyncio
-import uuid  # Import the uuid module
-
+import uuid
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from utils import save_media
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'
-socketio = SocketIO(app)
 
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = True
 app.config['SESSION_USE_SIGNER'] = True
-# app.config["PERMANENT_SESSION_LIFETIME"] = 300
-app.config['UPLOAD_FOLDER'] = './files/uploads'
-app.config['OUTPUT_FOLDER'] = './files/outputs'
 
-# Global dictionary to store selected formats
-selected_formats = {}
+app.config['SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'
 
-def get_file_list(folder):
-    return [{'id': id, 'filename': filename} for id, filename in enumerate(os.listdir(folder), 1)]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-async def convert_file_async(input_file_path, output_file_path, selected_format, user_id):
-    loop = asyncio.get_running_loop()
-    await loop.run_in_executor(None, convert_file_sync, input_file_path, output_file_path, selected_format, user_id)
-
-def convert_file_sync(input_file_path, output_file_path, selected_format, user_id):
-    fileExt = str(input_file_path).split('.')[-1].upper()
-    print(fileExt)
-    if selected_format != fileExt:
-            if selected_format == str("docx").upper():
-                socketio.emit('flash_message', {'user_id': user_id, 'message': 'Conversion in progress...'})
-                cv = Converter(input_file_path)
-                cv.convert(output_file_path, start=0, end=None)
-                cv.close()
-                socketio.emit('flash_message', {'user_id': user_id, 'message': 'File Successfully Converted!'})
-            else:
-                print("select docx")
-    else:
-        socketio.emit('flash_message', {'user_id': user_id, 'message': f"Please select format and try again"})
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-def get_user_folder(user_id):
-    user_upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], f'user_{user_id}')
-    user_output_folder = os.path.join(app.config['OUTPUT_FOLDER'], f'user_{user_id}')
-    os.makedirs(user_upload_folder, exist_ok=True)
-    os.makedirs(user_output_folder, exist_ok=True)
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    post_text = db.Column(db.String(255), nullable=False)
+    post_media = db.Column(db.String(255), nullable=True)  # Changed from post_video_link
+    is_published = db.Column(db.Boolean, default=False)
+
+    def __init__(self, post_text, post_media=None, is_published=False):
+        self.post_text = post_text
+        self.post_media = post_media
+        self.is_published = is_published
+
 
 
 @app.route('/')
 def index():
     user_id = str(session.get('user_id', None)).replace('-', '')
+    # Get all published posts
+    published_posts = Post.query.filter_by(is_published=True).all()
+    # Get all unpublished posts
+    unpublished_posts = Post.query.filter_by(is_published=False).all()
+
     if user_id:
-        get_user_folder(user_id)
+        session['user_id'] = user_id
     else:
-        # Generate a unique user_id if not present in the session
         user_id = str(uuid.uuid4().hex)[:16].replace('-', '')
         session['user_id'] = user_id
-        get_user_folder(user_id)
+        
+    return render_template('index.html', user_id=user_id, published_posts=published_posts, unpublished_posts=unpublished_posts )
 
-    files = get_file_list(f"./files/uploads/user_{user_id}")
-    outputs = get_file_list(f"./files/outputs/user_{user_id}")
-
-    return render_template('index.html', user_id=user_id, files=files, outputs=outputs)
-
-
-# Add this route to your Flask app
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 @app.route('/add', methods=['POST'])
-def add():
-    user_id = str(session.get('user_id', None)).replace('-', '')  # Default user_id if not set
+def add_post():
+    post_text = request.form['post_text']
+    post_media = request.files['post_media']  # Update from post_video
 
-    file = request.files.get('file')
+    if post_media.filename == '' and post_text == '':
+        flash("No content provided.", 'failed')
+        return redirect(url_for('index'))
 
-    if file:
-        upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], f'user_{user_id}')
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(upload_folder, filename))
-        flash("File Uploaded Successfully")
-
-    return redirect(url_for('index'))
-
-@app.route('/convert/<int:file_id>')
-async def convert(file_id):
-
-    user_id = str(session.get('user_id', None)).replace('-', '')
-    upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], f'user_{user_id}')
-    output_folder = os.path.join(app.config['OUTPUT_FOLDER'], f'user_{user_id}')
-
-    files = get_file_list(upload_folder)
-    input_file_path = os.path.join(upload_folder, files[file_id - 1]['filename'])
-    output_file_path = os.path.join(output_folder, files[file_id - 1]['filename'].replace(".pdf", ".docx"))
-
-    # Fetch the selected format from the global dictionary
-    selected_format = selected_formats.get(str(file_id))
-
-    if selected_format:
-        try:
-            # Use the selected format in your conversion logic
-            print(selected_format)
-            await convert_file_async(input_file_path, output_file_path, selected_format, user_id)
-        except Exception as e:
-            socketio.emit('flash_message', {'user_id': user_id, 'message': f'Error during conversion: {str(e)}'})
+    if post_media.filename == '':
+        media_path = ""
     else:
-        socketio.emit('flash_message', {'user_id': user_id, 'message': 'Error fetching selected format'})
+        media_path = save_media(post_media)  # Update the saving function for media
+
+    new_post = Post(post_text=post_text, post_media=media_path, is_published=False)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    flash("Post Created Successfully", 'success')
 
     return redirect(url_for('index'))
 
 
-@app.route('/download/<int:file_id>')
-def download(file_id):
-    user_id = str(session.get('user_id', None)).replace('-', '')
-    output_folder = os.path.join(app.config['OUTPUT_FOLDER'], f'user_{user_id}')
+@app.route('/delete/<int:post_id>', methods=['GET','POST'])
+def delete_post(post_id):
+    # Query the post by its ID
+    post = Post.query.get_or_404(post_id)
 
     try:
-        files = get_file_list(output_folder)
-        file_path = os.path.join(output_folder, files[file_id - 1]['filename'])
-        flash('File Downloaded Successfully')
-        return send_file(file_path, as_attachment=True)
-    except IndexError:
-        flash("Error Downloading File")
-        return "File not found", 404
+        # Delete the post from the database
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted successfully.', 'success')
+    except:
+        db.session.rollback()
+        flash('An error occurred while deleting the post.', 'error')
+
+    # Redirect to the home page or wherever you want after deleting the post
+    return redirect(url_for('index'))
 
 
-@app.route('/delete/<int:file_id>')
-def delete(file_id):
-    user_id = str(session.get('user_id', None)).replace('-', '')
-    upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], f'user_{user_id}')
+@app.route('/edit/<int:post_id>', methods=['POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
 
-    try:
-        files = get_file_list(upload_folder)
-        file_path = os.path.join(upload_folder, files[file_id - 1]['filename'])
-        os.remove(file_path)
-        flash("File Deleted Successfully")
+    if request.method == 'POST':
+        post_text = request.form['post_text']
+        post_media = request.files['post_media']  # Update from post_video
 
-    except (IndexError, OSError) as e:
-        flash("Error Deleting File")
-        return f"Error deleting file: {str(e)}", 500
+        post.post_text = post_text
+
+        if post_media.filename != '':
+            media_path = save_media(post_media)  # Update the saving function for media
+            post.post_media = media_path
+
+        db.session.commit()
+
+        flash("Post updated successfully", 'success')
+
+        return redirect(url_for('index'))
 
     return redirect(url_for('index'))
 
 
-@app.route('/delete_converted/<int:file_id>')
-def delete_converted(file_id):
-    user_id = str(session.get('user_id', None)).replace('-', '')
-    output_folder = os.path.join(app.config['OUTPUT_FOLDER'], f'user_{user_id}')
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
-    try:
-        output_files = get_file_list(output_folder)
-        file_path = os.path.join(output_folder, output_files[file_id - 1]['filename'])
-        os.remove(file_path)
-        flash("File Deleted Successfully")
-    except (IndexError, OSError) as e:
-        flash("Error Deleting File")
-        return f"Error deleting converted file: {str(e)}", 500
-
-    return redirect(url_for('index'))
-
-@app.route('/update_format', methods=['POST'])
-def update_format():
-    selected_format = request.form['selected_format']
-    print(selected_format)
-    file_id = request.form['file_id']
-    print(selected_format, file_id)
-
-    # Update the selected format in the global dictionary
-    selected_formats[file_id] = selected_format
-    return 'Format updated successfully'
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True, use_reloader=False)
-
+    db.create_all()
+    app.run(debug=True)
+    # socketio.run(app, debug=True, use_reloader=False)
---
+++ utils.py
@@ -0,0 +1,14 @@
+import os
+from werkzeug.utils import secure_filename
+
+def save_media(media_file):
+    # Ensure the uploads directory exists
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+
+    # Save the media file to the uploads directory
+    filename = secure_filename(media_file.filename)
+    media_path = os.path.join('uploads', filename)
+    media_file.save(media_path)
+    
+    return media_path
