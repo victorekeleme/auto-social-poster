edb83678ebf3c25685b06def3683571cb7e5d36b
Victor Ekeleme <ekelemevictor99@gmail.com>
2024-03-31 00:28:23 -0400

    fixed login issues

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file, flash, session
+from flask import Flask, render_template, request, redirect, url_for, send_file, flash, session, abort
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_socketio import SocketIO, emit
@@ -6,7 +6,7 @@ import asyncio
 import uuid
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from utils import save_media
 from datetime import datetime
 
@@ -26,26 +26,34 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 
 
-class Post(db.Model):
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    post_text = db.Column(db.String(255), nullable=False)
-    post_media = db.Column(db.String(255), nullable=True)
-    is_published = db.Column(db.Boolean, default=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    def __init__(self, post_text, post_media=None, is_published=False):
-        self.post_text = post_text
-        self.post_media = post_media
-        self.is_published = is_published
-
 # User model for authentication
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
-    settings = db.relationship('Settings', backref='user', uselist=False)
+    posts = db.relationship('Post', backref='users_post', lazy=True)
+
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_text = db.Column(db.String(255), nullable=False)
+    post_media = db.Column(db.String(255), nullable=True)
+    is_published = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __init__(self, post_text, post_media=None, is_published=False, user_id=None):
+        self.post_text = post_text
+        self.post_media = post_media
+        self.is_published = is_published
+        self.user_id = user_id
+
 
 # Define Settings model with foreign key to User
 class Settings(db.Model):
@@ -61,6 +69,12 @@ class Settings(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+# Define unauthorized handler to redirect users to the login page
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    flash("You must be logged in to access this page.", "error")
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -79,6 +93,7 @@ def register():
         # Create a new user
         new_user = User(username=username, password_hash=hashed_password)
         db.session.add(new_user)
+
         db.session.commit()
 
         flash('User registered successfully. Please log in.', 'success')
@@ -112,40 +127,41 @@ def logout():
 
 
 @app.route('/')
+@login_required  # Ensure only logged-in users can access this route
 def index():
-    user_id = str(session.get('user_id', None)).replace('-', '')
+    user = current_user  # Get the current logged-in user object
+    
     # Get all published posts
-    published_posts = Post.query.filter_by(is_published=True).all()
+    published_posts = Post.query.filter_by(user_id=user.id, is_published=True).all()
     # Get all unpublished posts
-    unpublished_posts = Post.query.filter_by(is_published=False).all()
+    unpublished_posts = Post.query.filter_by(user_id=user.id, is_published=False).all()
 
-    if user_id:
-        session['user_id'] = user_id
-    else:
-        user_id = str(uuid.uuid4().hex)[:16].replace('-', '')
-        session['user_id'] = user_id
-        
-    return render_template('index.html', user_id=user_id, published_posts=published_posts, unpublished_posts=unpublished_posts )
+    return render_template('index.html', user=user, published_posts=published_posts, unpublished_posts=unpublished_posts)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/add', methods=['POST'])
+@login_required
 def add_post():
     post_text = request.form['post_text']
-    post_media = request.files['post_media']  # Update from post_video
+    post_media = request.files['post_media']
+
+    user_id = current_user.id
+    user = User.query.filter_by(id=user_id).first()
 
     if post_media.filename == '' and post_text == '':
         flash("No content provided.", 'failed')
         return redirect(url_for('index'))
 
-    if post_media.filename == '':
-        media_path = ""
-    else:
-        media_path = save_media(post_media)  # Update the saving function for media
+    media_path = ""
+    if post_media.filename != '':
+        media_path = save_media(post_media)
 
-    new_post = Post(post_text=post_text, post_media=media_path, is_published=False)
+    new_post = Post(post_text=post_text, post_media=media_path, is_published=False, user_id=user.id)
+
 
     db.session.add(new_post)
     db.session.commit()
@@ -155,79 +171,50 @@ def add_post():
     return redirect(url_for('index'))
 
 
-@app.route('/delete/<int:post_id>', methods=['GET','POST'])
+@app.route('/delete/<int:post_id>', methods=['GET', 'POST'])
+@login_required
 def delete_post(post_id):
-    # Query the post by its ID
-    post = Post.query.get_or_404(post_id)
+    post = Post.query.filter_by(id=post_id, user_id=current_user.id).first_or_404()
 
     try:
-        # Delete the post from the database
         db.session.delete(post)
         db.session.commit()
         flash('Post deleted successfully.', 'success')
-    except:
+    except Exception as e:
         db.session.rollback()
         flash('An error occurred while deleting the post.', 'error')
 
-    # Redirect to the home page or wherever you want after deleting the post
     return redirect(url_for('index'))
 
 
 @app.route('/edit/<int:post_id>', methods=['POST'])
+@login_required
 def edit_post(post_id):
-    post = Post.query.get_or_404(post_id)
+    post = Post.query.filter_by(id=post_id, user_id=current_user.id).first_or_404()
 
     if request.method == 'POST':
         post_text = request.form['post_text']
-        post_media = request.files['post_media']  # Update from post_video
+        post_media = request.files['post_media']
 
         post.post_text = post_text
 
         if post_media.filename != '':
-            media_path = save_media(post_media)  # Update the saving function for media
+            media_path = save_media(post_media)
             post.post_media = media_path
 
         db.session.commit()
 
         flash("Post updated successfully", 'success')
 
-        return redirect(url_for('index'))
-
     return redirect(url_for('index'))
 
 
-# @app.route('/settings', methods=['GET', 'POST'])
-# def settings():
-
-#     if request.method == 'POST':
-#         # Process form data
-#         posts_per_day = request.form.get('postsPerDay')
-#         weekly_schedule = ','.join(request.form.getlist('weeklySchedule'))
-#         recycle_post_after = request.form.get('recyclePostAfter')
-        
-#         # Do something with the settings data, such as updating the database
-#         new_settings = Settings(posts_per_day=posts_per_day, weekly_schedule=weekly_schedule, recycle_post_after=recycle_post_after)
-        
-#         db.session.add(new_settings)
-#         db.session.commit()
-
-#         # For demonstration purposes, let's just print the received data
-#         print("Posts Per Day:", posts_per_day)
-#         print("Weekly Schedule:", weekly_schedule)
-#         print("Recycle Post After:", recycle_post_after)
-        
-#         flash("Settings saved successfully", 'success')
-#         # Redirect to a success page or render a template
-#         return redirect(url_for('settings'))
-#     else:
-#         # Render the settings page template
-#         return render_template('settings.html')
-
-
 @app.route('/settings', methods=['GET', 'POST'])
+@login_required
 def settings():
-    user_id = session.get('user_id')
+    user_id = current_user.id
     user = User.query.filter_by(id=user_id).first()  # Retrieve the user object from the database
+    settings = Settings.query.filter_by(user_id=user_id).first()
 
     if request.method == 'POST':
         # Process form data
@@ -235,21 +222,27 @@ def settings():
         weekly_schedule = ','.join(request.form.getlist('weeklySchedule'))
         recycle_post_after = request.form.get('recyclePostAfter')
 
-        # Update the user's settings if they exist, otherwise create new settings
-        if user.settings:
-            user.settings.posts_per_day = posts_per_day
-            user.settings.weekly_schedule = weekly_schedule
-            user.settings.recycle_post_after = recycle_post_after
+        # Get user's existing settings if they exist
+        settings = Settings.query.filter_by(user_id=user_id).first()
+
+        if settings:
+            # Update settings
+            settings.posts_per_day = posts_per_day
+            settings.weekly_schedule = weekly_schedule
+            settings.recycle_post_after = recycle_post_after
         else:
-            new_settings = Settings(posts_per_day=posts_per_day, weekly_schedule=weekly_schedule, recycle_post_after=recycle_post_after)
-            user.settings = new_settings
+            # Create new settings for the user
+            settings = Settings(user_id=user_id, posts_per_day=posts_per_day, weekly_schedule=weekly_schedule, recycle_post_after=recycle_post_after)
+            db.session.add(settings)
+
+            print(settings.posts_per_day, settings.weekly_schedule, settings.recycle_post_after, settings.user_id)
 
         db.session.commit()
 
         flash("Settings saved successfully", 'success')
         return redirect(url_for('settings'))
     else:
-        return render_template('settings.html', user=user)
+        return render_template('settings.html', user=user, settings=settings)
 
 
 
